2e4f991b59d7dc5196cecf8b144887bd1cead46a
AsrarAlmarri <76258365+AsrarAlmarri@users.noreply.github.com>
2021-01-04 00:25:35 +0300

    finally
    finished earley but tried to do the bonus ,, no luck

--- grade.js
+++ grade.js
@@ -1,17 +1,63 @@
 
-function calculator ()
+
+function submit()
+
 {
-let quiz = document.getElementById("Quizes").Value;
-let mid = document.getElementById("Midterm").Value;
-let ft = document.getElementById("FinalTest").Value;
-let oral = document.getElementById("Oral").Value;
 
-let total = (quiz+mid+ft+oral);
+    let quiz = document.getElementById(`Quizes`).value;
+ 
 
-let grade = document.getElementById("total_grades");
+    let mid = document.getElementById(`Midterm`).value;
+    
+    let finals = document.getElementById(`FinalTest`).value;
+    
+    let oral = document.getElementById(`Oral`).value;
 
-let submit= formatText(total);
+    let a = document.getElementById("A");
 
-grade.innerHTML = submit;
+    // let b = document.getElementById("B");
 
-}
+    // let c = document.getElementById("C");
+
+    // let d = document.getElementById("D");
+
+    // let f = document.getElementById("F");
+
+   let calc =(quiz*100)+(mid*100)+(finals*100)+(oral*100);
+
+let finalG = calc/400;
+
+let total = document.getElementById('total_grades');
+total.innerText =`${finalG}`;
+
+if( finalG == 100 || finalG >=90)
+{
+    document.getElementById(`final_grade`).innerText = "A";
+    
+    
+    
+     
+    a.setAttribute("src", "./A.gif");
+
+  }
+  else if( finalG >= 80)
+  {
+   document.getElementById(`final_grade`).innerText = "B";
+
+  }
+  else if (finalG>= 70)
+  {
+   document.getElementById(`final_grade`).innerText =" C";
+
+  }
+  else if (finalG >= 60)
+  {
+      document.getElementById(`final_grade`).innerText =" D";
+  }
+
+  else
+  {
+   document.getElementById(`final_grade`).innerText =" F ";
+  }
+
+ }
